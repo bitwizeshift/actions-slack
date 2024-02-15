8c0da1e68a5e137b034b21a9c81ff09c04e64323
Matthew Rodusek <7519129+bitwizeshift@users.noreply.github.com>
2024-02-14 23:47:47 -0500

    🚦 Enable commenting in slack to test action

---
+++ chat/post-message/src/main.js
@@ -0,0 +1,55 @@
+const core = require('@actions/core');
+const slack = require("@slack/web-api");
+
+async function run() {
+  try {
+    // Required inputs
+    const token = core.getInput('slack-token', { required: true });
+    const channel = core.getInput('channel', { required: true });
+    const message = core.getInput('message', { required: true });
+
+    // Optional inputs
+    const icon_emoji   = core.getInput('icon-emoji', { required: false });
+    const icon_url     = core.getInput('icon-url', { required: false });
+    const username     = core.getInput('username', { required: false });
+    const mrkdwn       = core.getBooleanInput('mrkdwn', { required: false });
+    const unfurl_media = core.getBooleanInput('unfurl-media', { required: false });
+    const unfurl_links = core.getBooleanInput('unfurl-links', { required: false });
+    const thread_ts    = core.getInput('thread-ts', { required: false });
+    const parse        = core.getInput('parse', { required: false });
+
+    const client = new slack.WebClient(token, {
+      logLevel: slack.LogLevel.WARN
+    });
+    const result = await client.chat.postMessage({
+      text: message,
+      channel: channel,
+      token: token,
+      icon_emoji: icon_emoji || undefined,
+      icon_url: icon_url || undefined,
+      username: username || undefined,
+      unfurl_media: unfurl_media,
+      unfurl_links: unfurl_links,
+      mrkdwn: mrkdwn,
+      thread_ts: thread_ts || undefined,
+    })
+    if (!result.ok) {
+      result.errors.forEach((err) => {
+        core.error(err);
+      })
+      if (result.error) {
+        core.error(result.error);
+      }
+      core.setFailed(`an error occurred posting to Slack channel ${channel}`);
+    } else {
+      core.setOutput('channel', result.channel);
+      core.setOutput('ts', result.ts);
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+module.exports = {
+  run
+}
